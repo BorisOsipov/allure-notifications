8e3f28114e6e5720bc0ff7b5a987809ccfff91d7
mychko <v.mychko@premiumitsolution.com>
2021-11-04 21:17:31 +0300

    fix legend font size

--- src/main/java/guru/qa/allure/notifications/chart/ChartLegend.java
+++ src/main/java/guru/qa/allure/notifications/chart/ChartLegend.java
@@ -1,6 +1,7 @@
 package guru.qa.allure.notifications.chart;
 
 import org.knowm.xchart.PieChart;
+import java.awt.Font;
 
 import static java.awt.Color.WHITE;
 import static org.knowm.xchart.style.Styler.LegendLayout.Vertical;
@@ -12,6 +13,7 @@ public class ChartLegend {
                 .setLegendVisible(true)
                 .setLegendPosition(OutsideE)
                 .setLegendPadding(8)
+                .setLegendFont(new Font("Helvetica", Font.PLAIN, 18))
                 .setLegendBorderColor(WHITE)
                 .setLegendLayout(Vertical);
     }
